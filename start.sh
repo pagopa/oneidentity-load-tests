7d4224e4e31d36476c5a562bb3269664ac02e729
BenitoVisone <visonebenito@gmail.com>
2024-10-28 11:46:36 +0100

    fix: start.sh remove cloudwatch agent start

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-# Start cloudwatch agent
-
 mkdir -p /root/.aws
 
 
@@ -11,12 +9,3 @@ aws_session_token = $AWS_SESSION_TOKEN
 aws_session_token = $AWS_SESSION_TOKEN
 region = eu-south-1
 EOF
-
-
-# Start cloudwatch agent.
-/opt/aws/amazon-cloudwatch-agent/bin/start-amazon-cloudwatch-agent&
-sleep 10
-K6_STATSD_ENABLE_TAGS=true k6 run --out statsd $1
-
-sleep 60
-cat /opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log

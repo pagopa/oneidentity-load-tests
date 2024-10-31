9f3f57ccec09afc68f8d018bbe4fb1b30d1f58df
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-10-31 07:51:12 +0100

    artillery

--- src/playwright.js
+++ src/playwright.js
@@ -1,13 +1,20 @@
-// npx playwright test --headed --workers=2 --repeat-each=2
-const { test, expect } = require('@playwright/test');
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://dev.oneid.pagopa.it/login?response_type=CODE&scope=openid&client_id=8U61NU_F8NMFDnTdW5zUt04MJ7wYRD_WgQbihbeenFg&state=dev&nonce=11701879fd734d1486b8a6435025b465&redirect_uri=https://442zl6z6sbdqprefkazmp6dr3y0nmnby.lambda-url.eu-south-1.on.aws/client/cb');
-  //await page.waitForURL('**/login**');
-  // Clicca sul pulsante "Entra con SPID"
-  await page.locator('#spidButton').click();
-  await expect(page.getByText('Scegli il tuo Identity Provider')).toBeVisible({ timeout: 1000000 });
-  await page.locator('[id="https://koz3yhpkscymaqgp4m7ceguu6m0tffuz.lambda-url.eu-south-1.on.aws"]').click();
-  //await expect(page).toHaveText(/Token e Codici/);  
-  await expect(page.getByText('Token e Codici')).toBeVisible({ timeout: 1000000 });
-});
+async function oi(page, userContext, events, test) {
+    await test.step('Login', async () => {
+      const requestPromise = page.waitForRequest('https://dev.oneid.pagopa.it/login?response_type=CODE&scope=openid&client_id=8U61NU_F8NMFDnTdW5zUt04MJ7wYRD_WgQbihbeenFg&state=dev&nonce=11701879fd734d1486b8a6435025b465&redirect_uri=https://442zl6z6sbdqprefkazmp6dr3y0nmnby.lambda-url.eu-south-1.on.aws/client/cb');
+      await page.goto('https://dev.oneid.pagopa.it/login?response_type=CODE&scope=openid&client_id=8U61NU_F8NMFDnTdW5zUt04MJ7wYRD_WgQbihbeenFg&state=dev&nonce=11701879fd734d1486b8a6435025b465&redirect_uri=https://442zl6z6sbdqprefkazmp6dr3y0nmnby.lambda-url.eu-south-1.on.aws/client/cb');
+      const req = await requestPromise;
+    });
+    await test.step('SPID Click', async () => {
+      await page.locator('#spidButton').click();
+    });
+  
+    await test.step('IdP Click', async () => {
+      await page.locator('[id="https://koz3yhpkscymaqgp4m7ceguu6m0tffuz.lambda-url.eu-south-1.on.aws"]').click();
+      const element = await page.waitForSelector('text=Token e Codici');
+     // await expect(element).toBeVisible();
+    });
+    }
+  
+  module.exports = {
+    oi
+  };

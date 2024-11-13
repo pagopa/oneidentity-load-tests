cd4c0ab31b954cc8baad8a4b67a9cabdca0660c4
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-11-13 17:24:30 +0100

    update-clientid

--- src/artilleryplaywright.js
+++ src/artilleryplaywright.js
@@ -1,7 +1,6 @@
 async function oi(page, userContext, events, test) {
   await test.step('Login', async () => {
-    //const requestPromise = page.waitForRequest('https://dev.oneid.pagopa.it/login?response_type=CODE&scope=openid&client_id=8U61NU_F8NMFDnTdW5zUt04MJ7wYRD_WgQbihbeenFg&state=dev&nonce=11701879fd734d1486b8a6435025b465&redirect_uri=https://442zl6z6sbdqprefkazmp6dr3y0nmnby.lambda-url.eu-south-1.on.aws/client/cb');
-    await page.goto('https://dev.oneid.pagopa.it/login?response_type=CODE&scope=openid&client_id=8U61NU_F8NMFDnTdW5zUt04MJ7wYRD_WgQbihbeenFg&state=dev&nonce=11701879fd734d1486b8a6435025b465&redirect_uri=https://442zl6z6sbdqprefkazmp6dr3y0nmnby.lambda-url.eu-south-1.on.aws/client/cb', { timeout: 120000 });
+    await page.goto('https://dev.oneid.pagopa.it/login?response_type=CODE&scope=openid&client_id=bxMiPVktuZ5lBNbZYJ3ODosXL57ltrLp7BgyOkw-0v4&state=dev&nonce=11701879fd734d1486b8a6435025b465&redirect_uri=https://442zl6z6sbdqprefkazmp6dr3y0nmnby.lambda-url.eu-south-1.on.aws/client/cb', { timeout: 120000 });
     //const req = await requestPromise;
   });
   await test.step('SPID Click', async () => {

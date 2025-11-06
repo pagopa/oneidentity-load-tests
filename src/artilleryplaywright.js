92670c930e9ffb07bc3f8b0d144e313446c48c0d
Alessandro Sebastiani <sebbalex@gmail.com>
2025-11-06 11:32:04 +0100

    test: 08 ramp test - refactor: package scripts - fix: wait for lambda product

--- src/artilleryplaywright.js
+++ src/artilleryplaywright.js
@@ -1,19 +1,28 @@
 async function oi(page, userContext, events, test) {
-  await test.step('Login', async () => {
-    await page.goto('https://dev.oneid.pagopa.it/login?response_type=CODE&scope=openid&client_id=bxMiPVktuZ5lBNbZYJ3ODosXL57ltrLp7BgyOkw-0v4&state=dev&nonce=11701879fd734d1486b8a6435025b465&redirect_uri=https://442zl6z6sbdqprefkazmp6dr3y0nmnby.lambda-url.eu-south-1.on.aws/client/cb', { timeout: 120000 });
-    //const req = await requestPromise;
+  const { step } = test;
+
+  await step("Login", async () => {
+    await page.goto(
+      "https://dev.oneid.pagopa.it/login?response_type=CODE&scope=openid&client_id=bxMiPVktuZ5lBNbZYJ3ODosXL57ltrLp7BgyOkw-0v4&state=dev&nonce=11701879fd734d1486b8a6435025b465&redirect_uri=https://442zl6z6sbdqprefkazmp6dr3y0nmnby.lambda-url.eu-south-1.on.aws/client/cb",
+      { timeout: 5000 }
+    );
   });
-  await test.step('SPID Click', async () => {
-    await page.locator('#spidButton').click({ timeout: 120000 });
+  await step("SPID Click", async () => {
+    await page.locator("#spidButton").click({ timeout: 5000 });
   });
 
-  await test.step('IdP Click', async () => {
-    await page.locator('[id="https://5ucp2co2zvqle6tcyrx4i5se7q0xdkni.lambda-url.eu-south-1.on.aws"]').click({ timeout: 120000 });
-    const element = await page.waitForSelector('text=Token e Codici');
-   // await expect(element).toBeVisible();
+  await step("IdP Click", async () => {
+    await page
+      .locator(
+        '[id="https://5ucp2co2zvqle6tcyrx4i5se7q0xdkni.lambda-url.eu-south-1.on.aws"]'
+      )
+      .click({ timeout: 5000 });
+    await page.waitForURL(
+      "https://442zl6z6sbdqprefkazmp6dr3y0nmnby.lambda-url.eu-south-1.on.aws/client/cb**"
+    );
   });
-  }
+}
 
 module.exports = {
-  oi
+  oi,
 };
